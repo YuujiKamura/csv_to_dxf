44b2b3674e9c6bd820b72d7be289d2d56594018c
yuuji <yuujikamura@gmail.com>
2024-05-18 19:24:10 +0900

    version1.25 load from clipboard

--- csv_to_dxf.py
+++ csv_to_dxf.py
@@ -1,16 +1,19 @@
 import ezdxf
-import pandas as pd
-import pyperclip
+import loader
 import dxf_draw_tenkaiz
 
 def main():
-    csv_path = 'data.csv'
-    dxf_path = 'road_sections_with_names_and_dimensions.dxf'
-    data = pd.read_csv(csv_path)
+    dxf_path = 'road_sections.dxf'
+
+    data = loader.load_data_from_clipboard()
     doc  = ezdxf.new(dxfversion="R2010")
+
     dxf_draw_tenkaiz.draw_road_sections( doc.modelspace(), data )
+
     doc.saveas(dxf_path)
-    print(f"DXF file has been saved as {dxf_path}")
+    message = f"\nDXF file has been saved as {dxf_path}"
+    loader.show_data_in_dialog(data, message)
+    print(message)
 
 if __name__ == "__main__":
     main()
--- dxf_draw_tenkaiz.py
+++ dxf_draw_tenkaiz.py
@@ -1,6 +1,5 @@
 from ezdxf.enums import TextEntityAlignment
 
-
 def draw_road_sections(msp, data):
     """道路断面と測点ラベルを描画する"""
     prev_points = (None,None,None)
@@ -40,22 +39,28 @@ def draw_gaikeisen(left_point, msp, previous_left_point, previous_right_point, r
             msp.add_line(previous_right_point, right_point)
 
 def draw_set_of_dimensions(data, index, msp, row):
-    name, x, wl, wr = row['name'], row['x'], row['wl'], row['wr']
-    prev_x = 0
+    name, added_distance, wl, wr = row['name'], row['x'], row['wl'], row['wr']
+    prev_x = -1
     if index > 0:
         prev_x = data.iloc[index - 1]['x']
+    alignment = align_by_distance(added_distance,prev_x)
+
     # 延長寸法を描画
-    if x - prev_x > 0.0:
-        add_text(msp, f"{x - prev_x:.2f}", ((x + prev_x) / 2, 0))
-    if x - prev_x > 1.0:
-        draw_sokuten(msp, name, wl, wr, x)
-    alignment = TOP_CENTER
-    if x - prev_x < 1.0:
-        alignment = BOTTOM_CENTER
+    if added_distance - prev_x > 1.0:
+        add_text(msp, f"{added_distance - prev_x:.2f}", ((added_distance + prev_x) / 2, 0))
+
+    draw_sokuten(msp, name, wl, wr, added_distance, prev_x, alignment)
+
     # 左側の幅員寸法を描画
-    draw_positive_dimension(msp, wl, (x, wl / 2), -90, alignment)
+    draw_positive_dimension(msp, wl, (added_distance, wl * 0.5 ), -90, alignment)
     # 右側の幅員寸法を描画
-    draw_positive_dimension(msp, wr, (x, -wr / 2), -90, alignment)
+    draw_positive_dimension(msp, wr, (added_distance, -wr * 0.5 ), -90, alignment)
+
+def align_by_distance(x, prev_x):
+    if x - prev_x < 1:
+        return BOTTOM_CENTER
+    else:
+        return TOP_CENTER
 
 TOP_CENTER=TextEntityAlignment.TOP_CENTER
 BOTTOM_CENTER=TextEntityAlignment.BOTTOM_CENTER
@@ -71,6 +76,7 @@ def add_text(msp, text, position, rotation=0, alignment=TOP_CENTER):
     dimension_text.dxf.align_point = position
     dimension_text.set_placement(position, align=alignment)
 
-def draw_sokuten(msp, name, wl, wr, x, alignment=TOP_CENTER):
-    # 測点ラベルを追加、-90度回転して上側に配置
-    add_text(msp, name, (x, max(wl, -wr) + 5), -90, alignment)
+def draw_sokuten(msp, name, wl, wr, x, prev_x, alignment=TOP_CENTER):
+    if x - prev_x > 0:
+        # 測点ラベルを追加、-90度回転して上側に配置
+        add_text(msp, name, (x, max(wl, -wr) + 5), -90, alignment)
