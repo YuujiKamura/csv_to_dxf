f53fb21853bbf3f18df93db05cdea9624195a44c
yuuji <yuujikamura@gmail.com>
2024-05-18 10:59:31 +0900

    version1.2 core refactor

--- csv_to_dxf.py
+++ csv_to_dxf.py
@@ -47,12 +47,6 @@ def draw_gaikeisen(left_point, msp, previous_left_point, previous_right_point, r
         if right_point[1] < 0:
             msp.add_line(previous_right_point, right_point)
 
-def draw_sokuten(msp, name, wl, wr, x):
-    # 測点ラベルを追加、-90度回転して上側に配置
-    text = msp.add_text(name, dxfattribs={'height': 1.0, 'rotation': -90})
-    text.dxf.insert = (x, max(wl, -wr) + 5)  # 高い方の上側に配置
-    text.set_placement((x, max(wl, -wr) + 5), align=TextEntityAlignment.TOP_CENTER)
-
 def draw_all_dimensions(msp, data):
     """幅員と延長の寸法を描画する"""
     for index, row in data.iterrows():
@@ -60,26 +54,39 @@ def draw_all_dimensions(msp, data):
 
 def draw_set_of_dimensions(data, index, msp, row):
     name, x, wl, wr = row['name'], row['x'], row['wl'], row['wr']
+    prev_x = 0
+    if index > 0:
+        prev_x = data.iloc[index - 1]['x']
+    # 延長寸法を描画
+    if x - prev_x > 0.0:
+        add_dimension_text(msp, f"{x - prev_x:.2f}", ((x + prev_x) / 2, 0))
+    alignment = TOP_CENTER
+    if x - prev_x < 1.0:
+        alignment = BOTTOM_CENTER
     # 左側の幅員寸法を描画
     draw_positive_dimension(msp, wl, (x, wl / 2), -90)
     # 右側の幅員寸法を描画
     draw_positive_dimension(msp, wr, (x, -wr / 2), -90)
-    # 延長寸法を描画
-    if index > 0:
-        prev_x = data.iloc[index - 1]['x']
-        if x - prev_x > 0.0:
-            add_dimension_text(msp, f"{x - prev_x:.2f}", ((x + prev_x) / 2, 0))
 
-def draw_positive_dimension(msp, value, position, rotation):
+TOP_CENTER=TextEntityAlignment.TOP_CENTER
+BOTTOM_CENTER=TextEntityAlignment.BOTTOM_CENTER
+
+def draw_positive_dimension(msp, value, position, rotation, alignment=TOP_CENTER):
     if value > 0.0:
-        add_dimension_text(msp, f"{value:.2f}", position, rotation)
+        add_dimension_text(msp, f"{value:.2f}", position, rotation, alignment )
 
-def add_dimension_text(msp, text, position, rotation=0):
+def add_dimension_text(msp, text, position, rotation=0, alignment=TOP_CENTER):
     """寸法テキストを追加する"""
     dimension_text = msp.add_text(text, dxfattribs={'height': 1.0, 'rotation': rotation})
     dimension_text.dxf.insert = position
     dimension_text.dxf.align_point = position
-    dimension_text.set_placement(position, align=TextEntityAlignment.TOP_CENTER)
+    dimension_text.set_placement(position, align=alignment)
+
+def draw_sokuten(msp, name, wl, wr, x, alignment=TOP_CENTER):
+    # 測点ラベルを追加、-90度回転して上側に配置
+    text = msp.add_text(name, dxfattribs={'height': 1.0, 'rotation': -90})
+    text.dxf.insert = (x, max(wl, -wr) + 5)  # 高い方の上側に配置
+    text.set_placement((x, max(wl, -wr) + 5), align=alignment)
 
 def save_dxf_document(doc, dxf_path):
     """DXFドキュメントを保存する"""
