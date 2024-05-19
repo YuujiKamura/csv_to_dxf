cdc4b8c06c55b81f0239a6aa2ee467cbf138d922
yuuji <yuujikamura@gmail.com>
2024-05-19 21:44:45 +0900

    version1.27 error dialog

--- loader.py
+++ loader.py
@@ -4,33 +4,55 @@ from tkinter import messagebox, scrolledtext
 
 def load_data_from_clipboard():
     # クリップボードからデータを読み込む
+    global data
     try:
         data = pd.read_clipboard(header=None)
         data.columns = ["name", "x", "wl", "wr"]
-        print("クリップボードから読み込んだデータ:")
-        print(data)
         validate_data(data)
         return data
     except ValueError as e:
-        print(f"データの検証エラー: {e}")
+        show_error_dialog(show_usage())
+        print(f"入力エラー: {e}")
+        print("クリップボードから読み込んだデータ:")
+        print(data)
         return
     except Exception as e:
+        show_error_dialog(show_usage())
         print(f"予期しないエラーが発生しました: {e}")
+        print("クリップボードから読み込んだデータ:")
+        print(data)
         return
 
 def load_data_from_csv():
     csv_path = 'data.csv'
     return pd.read_csv(csv_path)
 
+def show_usage():
+    sample_data = [
+        ["測点", "追加距離", "左幅員", "右幅員"],
+        ["No.0", f"{0}", f"{3.50}", f"{3.35}"],
+        ["No.1", f"{10}", f"{3.40}", f"{3.35}"]
+    ]
+    # 各列を適切にタブ区切りで整形
+    sample_data_str = "\n".join(["\t".join(row) for row in sample_data])
+
+    message = ("\n\n<<つかいかた>>\n"
+               "excelなどの表計算ソフトを使って４列構成の表を作成し、\n"
+               "クリップボードにコピーした後に実行してください。\n\n"
+               f"{sample_data_str}\n\n"
+               "成功すると、同一ディレクトリにdxfファイルが作成されます。\n\n"
+               )
+    return message
+
 def validate_data(data):
+    message = show_usage()
+    show_data_in_dialog(data, message)
+
     if data.shape[1] != 4:
-        message = "データの列数が正しくありません。4列のデータが必要です。"
-        show_error_dialog(message)
         raise ValueError(message)
 
     # 1列目が文字列、他の列が数値であることを確認
-    if not all(data.iloc[:, 0].apply(lambda x: isinstance(x, str))):
-        message = "1列目は全て文字列である必要があります。"
+    if not all(data.iloc[:, 1].apply(lambda x: isinstance(x, str))):
         show_error_dialog(message)
         raise ValueError(message)
 
@@ -42,13 +64,13 @@ def show_error_dialog(message):
 
 def show_data_in_dialog(data, message):
     root = tk.Tk()
-    root.title("クリップボードから読み込んだデータ")
+    root.title("road_dxf_drawer")
 
     text_area = scrolledtext.ScrolledText(root, wrap=tk.WORD, width=60, height=20)
     text_area.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
 
-    text_area.insert(tk.INSERT, data.to_string(index=False))
     text_area.insert(tk.INSERT, message)
+    text_area.insert(tk.INSERT, data.to_string(index=False))
     text_area.configure(state='disabled')  # Read-only
 
     root.mainloop()
